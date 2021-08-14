a145251aa97b5836a4d0c48ed8ff467a20400d74
YoonSeongBin <9603226407@daum.net>
2021-08-14 16:57:54 +0900

    Uploads, and Express server

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 require("dotenv").config();
 
-import { ApolloServer } from "apollo-server";
-import schema from "./schema";
-import { getUser } from "./users/users.utils";
-
-const server = new ApolloServer({
-  schema,
-  context: async ({ req }) => {
-    return { loggedInUser: await getUser(req.headers.token) };
-  },
-});
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema";
+import { getUser, protectResolver } from "./users/users.utils";
+import { graphqlUploadExpress } from "graphql-upload";
+//import logger from "morgan";
 
 const PORT = process.env.PORT;
+const app = express();
 
-server
-  .listen(PORT)
-  .then(() =>
-    console.log(`🔥Server is running on http://localhost:${PORT}/🔥`)
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+      };
+    },
+  });
+
+  await server.start();
+
+  app.use(graphqlUploadExpress());
+  //app.use(logger("tiny"));
+  server.applyMiddleware({ app });
+  app.use("/static", express.static("uploads"));
+
+  await new Promise((r) => app.listen({ port: PORT }, r)).then(() =>
+    console.log(
+      `🚀 Server is running on http://localhost:${PORT}${server.graphqlPath} ✅`
+    )
   );
+};
+startServer();
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,41 +1,67 @@
+import { createWriteStream } from "fs";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import client from "../../client";
+import { protectedResolver } from "../users.utils";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, userName, email, password: newPassword },
-      { loggedInUser }
-    ) => {
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
-      }
-
-      const updatedUser = await client.user.update({
-        where: {
-          id: loggedInUser.id,
-        },
-        data: {
+    editProfile: protectedResolver(
+      async (
+        _,
+        {
           firstName,
           lastName,
           userName,
           email,
-          ...(uglyPassword && { password: uglyPassword }),
+          password: newPassword,
+          bio,
+          avatar,
         },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update profile",
-        };
+        { loggedInUser }
+      ) => {
+        let avatarUrl = null;
+        if (avatar) {
+          const { filename, createReadStream } = await avatar;
+          const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+          const readStream = createReadStream();
+          const writeStream = createWriteStream(
+            process.cwd() + "/uploads/" + newFilename
+          );
+          readStream.pipe(writeStream);
+          avatarUrl = `http://localhost:4000/static/${newFilename}`;
+          console.log(avatarUrl);
+        }
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            userName,
+            email,
+            bio,
+            ...(uglyPassword && { password: uglyPassword }),
+            ...(avatarUrl && { avatar: avatarUrl }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile",
+          };
+        }
       }
-    },
+    ),
   },
 };
